57c1e87a1b5f38821f93d9a8d833b17f5669c8c9
LMauricius <mauriciosmit100@gmail.com>
2025-04-27 17:58:52 +0200

    Initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+# VitraePluginGI
+
+An experimental GI done for my Master's thesis initially.
---
+++ src/Setup.cpp
@@ -0,0 +1,9 @@
+#include "VitraePluginGI/Setup.hpp"
+
+namespace VitraePluginGI
+{
+
+void setup(Vitrae::ComponentRoot &root) {
+}
+
+} // namespace VitraePluginEffects
