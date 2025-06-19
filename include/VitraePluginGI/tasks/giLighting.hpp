1a9feb436d2f4cc09c49aa0bcc4aab95cc5fce51
LMauricius <mauriciosmit100@gmail.com>
2025-06-19 11:53:45 +0200

    Using the new recursive structure in fragment lighting

--- include/VitraePluginGI/tasks/giLighting.hpp
+++ include/VitraePluginGI/tasks/giLighting.hpp
@@ -41,14 +41,42 @@ inline void setupGILighting(ComponentRoot &root)
     */
 
     methodCollection.registerShaderTask(
+        root.getComponent<ShaderHeaderKeeper>().new_asset({ShaderHeader::StringParams{
+            .inputSpecs =
+                {
+                    {"gpuProbeRecursions", TYPE_INFO<ProbeRecursionBufferPtr>},
+                },
+            .outputSpecs =
+                {
+                    {"declared_probe_search", TYPE_INFO<void>},
+                },
+            .snippet = R"glsl(
+                uint getDeepestProbe(vec3 pointPosition) {
+                    uint currentProbe = 0;
+
+                    while (gpuProbeRecursions[currentProbe].childIndex[0] != 0) {
+                        bvec3 childIndPos = lessThan(
+                            gpuProbeRecursions[currentProbe].pivot,
+                            pointPosition
+                        );
+                        currentProbe = gpuProbeRecursions[currentProbe].childIndex[
+                            (childIndPos.x? 4:0) + (childIndPos.y? 2:0) + (childIndPos.z? 1:0)
+                        ];
+                    }
+                    
+                    return currentProbe;
+                }
+            )glsl",
+        }}),
+        ShaderStageFlag::Fragment | ShaderStageFlag::Compute);
+
+    methodCollection.registerShaderTask(
         root.getComponent<ShaderSnippetKeeper>().new_asset({ShaderSnippet::StringParams{
             .inputSpecs =
                 {
+                    {"declared_probe_search", TYPE_INFO<void>},
                     {"gpuProbes", TYPE_INFO<ProbeBufferPtr>},
                     {"gpuProbeStates", TYPE_INFO<ProbeStateBufferPtr>},
-                    {"giWorldStart", TYPE_INFO<glm::vec3>},
-                    {"giWorldSize", TYPE_INFO<glm::vec3>},
-                    {"giGridSize", TYPE_INFO<glm::uvec3>},
                     {"position_world", TYPE_INFO<glm::vec4>},
                     {"normal_fragment_normalized", TYPE_INFO<glm::vec3>},
                 },
@@ -57,15 +85,8 @@ inline void setupGILighting(ComponentRoot &root)
                     {"shade_gi_ambient", TYPE_INFO<glm::vec3>},
                 },
             .snippet = R"glsl(
-                uvec3 gridPos = uvec3(floor(
-                    (position_world.xyz - giWorldStart) / giWorldSize * vec3(giGridSize)
-                ));
-                gridPos = clamp(gridPos, uvec3(0), giGridSize - uvec3(1));
-
-                uint ind = gridPos.x * giGridSize.y * giGridSize.z + gridPos.y * giGridSize.z + gridPos.z;
-                vec3 probeSize = gpuProbes[ind].size.xyz;//(giWorldSize / vec3(giGridSize));
-                vec3 probePos = gpuProbes[ind].position.xyz;//giWorldStart + (vec3(gridPos) + 0.5) * probeSize;
-
+                uint ind = getDeepestProbe(position_world.xyz / position_world.w);
+                
                 bvec3 normalIsNeg = lessThan(-normal_fragment_normalized, vec3(0.0));
                 vec3 absNormal = abs(normal_fragment_normalized);
 
